442604a43a83950b2123288ce17f1acd2b395342
begalievn <begaliev.info@gmail.com>
2022-12-13 05:06:18 +0600

    Added middleware to check for role

--- server/routes/typeRoute.js
+++ server/routes/typeRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const typeController = require('../controllers/typeController');
+const checkRole = require('../middleware/checkRoleMIddleware');
 
-router.post('/', typeController.create);
+router.post('/', checkRole('ADMIN'), typeController.create);
 router.get('/', typeController.getAll);
 
 module.exports = router;
